4070dd84bb9e9540d6204ceee4e39b432532513a
Olivia Frost <olivia@decoded.com>
2018-04-27 12:07:46 +0100

    added form to StorePicker.jsx

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,7 +2,14 @@ import React from 'react';
 
 class StorePicker extends React.Component {
   render() {
-    return <p>Hello</p>
+    return (
+      <form className="store-selector">
+        <h2>Please Enter a Store</h2>
+        <input type="text" required placeholder="Store Name"/>
+        <button type="submit">Visit Store --></button>
+        {/* we'll hook this up to functionality later */}
+      </form>
+    )
   }
 }
 
