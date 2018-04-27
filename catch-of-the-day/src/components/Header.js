97e20a48e092696d050ad7d4086ff4fc79c1c123
Olivia Frost <olivia@decoded.com>
2018-04-27 16:22:29 +0100

    changed Header.js to a stateless functional component as its only job is to update some html

--- catch-of-the-day/src/components/Header.js
+++ catch-of-the-day/src/components/Header.js
@@ -1,23 +1,43 @@
 import React from 'react';
 
-class Header extends React.Component{
-  render() {
-    console.log(this)
-    return (
-      <header className="top">
-        <h1>
-          Catch
-          <span className="ofThe">
-            <span className="of">of</span>
-            <span className="the">the</span>
-          </span>
-          Day
-        </h1>
-        <h3 className="tagline"><span>{this.props.tagline}</span></h3>
-        {/* 'this' refers to the actual component, in this case Header, props is an object that we can call .tagline on */}
-      </header>
-    )
-  }
+// Using a stateless functional component:
+const Header = (props) => {
+  return (
+    <header className="top">
+      <h1>
+        Catch
+        <span className="ofThe">
+          <span className="of">of</span>
+          <span className="the">the</span>
+        </span>
+        Day
+      </h1>
+      <h3 className="tagline"><span>{props.tagline}</span></h3>
+      {/* 'this' refers to the actual component, in this case Header, props is an object that we can call .tagline on */}
+    </header>
+  )
 }
 
 export default Header; 
+
+
+// As our component was before:
+// class Header extends React.Component{
+//   render() {
+//     console.log(this)
+//     return (
+//       <header className="top">
+//         <h1>
+//           Catch
+//           <span className="ofThe">
+//             <span className="of">of</span>
+//             <span className="the">the</span>
+//           </span>
+//           Day
+//         </h1>
+//         <h3 className="tagline"><span>{this.props.tagline}</span></h3>
+//         {/* 'this' refers to the actual component, in this case Header, props is an object that we can call .tagline on */}
+//       </header>
+//     )
+//   }
+// }
