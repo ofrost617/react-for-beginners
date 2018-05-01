e355e5653d70a49be89cbdaf5a0f1e9d13f7f62c
Olivia Frost <olivia@decoded.com>
2018-05-01 16:59:27 +0100

    added onSubmit event to get store name from input

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -4,12 +4,27 @@ import React from 'react';
 import { getFunName } from '../helpers'
 
 class StorePicker extends React.Component {
+  // we could sue the contrcutior of a component
+  // constructor() {
+  //   super();
+  //   this.goToStore = this.goToStore.bind(this);
+  // }
+  // Another way is to do that in line 24- onSubmit
+  goToStore(event) {
+    event.preventDefault()
+    // prevents the page from refreshing
+    console.log("You changed the URL");
+    // first grab the text from the box
+    console.log(this.storeInput.value)
+    // transition from / to /store.:storeId
+  }
+// render methods are bound to the component but other methods aren't eg the one above. This mand we cna use this within render 
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore.bind(this)}>
         <h2>Please Enter a Store</h2>
         {/* We use defaultValuse rather than value because the value attribute is tied to state. we use {} so that we can write JS inside */}
-        <input type="text" required placeholder="Store Name" defaultValue={getFunName()}/>
+        <input type="text" required placeholder="Store Name" defaultValue={getFunName()} ref={(input) => {this.storeInput = input}}/>
         <button type="submit">Visit Store --></button>
         {/* we'll hook this up to functionality later */}
       </form>
